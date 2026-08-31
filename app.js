2a085033f6f89db8dd7cb7f32e683aecee3c4d90
mohnadhhh90-arch <mohnadhhh90@gmail.com>
2026-08-31 14:47:47 +0300

    Add new monitor item to product list

--- app.js
+++ app.js
@@ -123,6 +123,25 @@ const defaultProducts = [
             "وقت استجابة سريع",
             "مداخل متعددة HDMI / DP"
         ]
+    },
+
+    {
+        id: 5,
+        name: "Monitor",
+        category: "Monitors",
+        price: 8499,
+        oldPrice: 9499,
+        image: "assets/monitor.jpg",
+        badge: "NEW",
+        stock: 10,
+        description: "شال تحديث سريع.",
+        specifications: [
+            "حجم 27نية",
+            "معدل تحديث 165Hz",
+            "دقة Full HD",
+            "وقت استجابة سريع",
+            "مداخل متعددة HDMI / DP"
+        ]
     }
 ];
 
